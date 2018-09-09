77bb13b1ffd5afcb0fc6893655574c4065564a48
Yukiya025 <myu.yukiyanagi@gmail.com>
2018-09-09 01:00:19 +0000

    first commit

---
+++ app/assets/images/README.txt
@@ -0,0 +1,2 @@
+They are downloaded from this page.
+https://drive.google.com/drive/folders/19eHGUcTUKfyMP_7SmswW6rGBdZ8h00cl
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root 'pages#home' 
+  devise_for :users 
+  resources :users, only: %i(show)
+end
